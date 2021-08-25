687b201e98558f19d0dcb3577ab27f5f886a370e
demarillacizere <izered3@gmail.com>
2021-08-25 14:24:33 +0200

    adjusted the links

--- inc/navbar.php
+++ inc/navbar.php
@@ -220,7 +220,7 @@ background-color: none;
       <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 35.6">
         <use xlink:href="#planner"></use></svg> -->
         <i class="far fa-user fa-lg"></i>
-      <a href="/Lost-item/login.php">Login<i class="fa fa-chevron-down"  style='left:5px;'></i></a>
+      <a href="login.php">Login<i class="fa fa-chevron-down"  style='left:5px;'></i></a>
       <!-- <div class="dropdown-content">
           <a class="dropdown-item" href="viewLostPosts.php">Admin</a>
           <a class="dropdown-item" href="viewFoundPosts.php">User</a>
