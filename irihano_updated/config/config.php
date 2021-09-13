eb07bc2a20102a5570837645abbc12cade258b86
lekipising <leeplanke@gmail.com>
2021-09-13 08:02:05 +0200

    update root url

--- irihano_updated/config/config.php
+++ irihano_updated/config/config.php
@@ -1,6 +1,6 @@
 <?php
-    define('ROOT_URL', 'http://localhost/lost-item/');
-    define('DB_HOST','us-cdbr-east-04.cleardb.com');
-    define('DB_USER', 'b21067b978ee58');
-    define('DB_PASS', '667b953a');
-    define('DB_NAME', 'heroku_67906ddf27425d2');
+define('ROOT_URL', 'https://irihanolostandfound.herokuapp.com/');
+define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+define('DB_USER', 'b21067b978ee58');
+define('DB_PASS', '667b953a');
+define('DB_NAME', 'heroku_67906ddf27425d2');
